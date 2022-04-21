12ffb0c3ebc01238055d8f3c634be832a9af3de3
younghun.yun <younghun29.yun@gmail.com>
2022-04-21 17:19:03 +0900

    commit 업데이트

---
+++ src/test/java/kr/bepsinlab/nivea/NiveaApplicationTests.java
@@ -0,0 +1,14 @@
+package kr.bepsinlab.nivea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+//@SpringBootTest
+class NiveaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
