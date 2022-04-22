05901cbb57d7f525a8a78d4dd0be96a9245e86e5
younghun.yun <younghun29.yun@gmail.com>
2022-04-22 11:16:00 +0900

    DB 환경설정 업데이트

--- db/data/celeb.sql
+++ db/data/celeb.sql
@@ -1,8 +1,28 @@
-INSERT INTO celebs(celeb_code, stage_name, stage_name_en, real_name, real_name_en, gender, birth_date, debut_date, country, region, height, weight, company, groups) VALUES(
-'SIN', '솔라', '', '김용선', 'Kim Yongsun', 'F', '1991-02-21', '2014-06-19', 'South Korea', 'Seoul', 163, 43, 'RBW', '[Mamamoo]');
-INSERT INTO celebs(celeb_code, stage_name, stage_name_en, real_name, real_name_en, gender, birth_date, debut_date, country, region, height, weight, company, groups) VALUES(
-'SIN', '화사', '', '안혜진', 'Ahn Hyejin', 'F', '1995-07-23', '2014-06-19', 'South Korea', 'Jeonju', '162', '44', 'RBW', '[Mamamoo]');
-INSERT INTO celebs(celeb_code, stage_name, stage_name_en, real_name, real_name_en, gender, birth_date, debut_date, country, region, height, weight, company, groups) VALUES(
-'SIN', '휘인', '', '정휘인', 'Jung Wheein', 'F', '1995-04-17', '2014-06-19', 'South Korea', 'Jeonju', '162', '43', 'RBW', '[Mamamoo]');
-INSERT INTO celebs(celeb_code, stage_name, stage_name_en, real_name, real_name_en, gender, birth_date, debut_date, country, region, height, weight, company, groups) VALUES(
-'SIN', '문별', '', '문별이', 'Moon Byulyi', 'F', '1992-12-22', '2014-06-19', 'South Korea', 'Bucheon', '165', '45', 'RBW', '[Mamamoo]');
+INSERT INTO celebs(celeb_code, stage_name, stage_name_en, real_name, real_name_en, gender, birth_date, debut_year, country, country_en, state, city, height, weight, company) VALUES(
+'SIN', '솔라', '', '김용선', 'Kim Yongsun', 'F', '1991-02-21', '2014', '대한민국', 'South Korea', '서울', '강서구', 163, 43, 'RBW');
+INSERT INTO celebs(celeb_code, stage_name, stage_name_en, real_name, real_name_en, gender, birth_date, debut_year, country, country_en, state, city, height, weight, company) VALUES(
+'SIN', '화사', '', '안혜진', 'Ahn Hyejin', 'F', '1995-07-23', '2014', '대한민국', 'South Korea', '전라북도', '전주시', 162, 44, 'RBW');
+INSERT INTO celebs(celeb_code, stage_name, stage_name_en, real_name, real_name_en, gender, birth_date, debut_year, country, country_en, state, city, height, weight, company) VALUES(
+'SIN', '휘인', '', '정휘인', 'Jung Wheein', 'F', '1995-04-17', '2014', '대한민국', 'South Korea', '전라북도', '전주시', 162, 43, 'RBW');
+INSERT INTO celebs(celeb_code, stage_name, stage_name_en, real_name, real_name_en, gender, birth_date, debut_year, country, country_en, state, city, height, weight, company) VALUES(
+'SIN', '문별', '', '문별이', 'Moon Byulyi', 'F', '1992-12-22', '2014', '대한민국', 'South Korea', '경기도', '부천시', 165, 45, 'RBW');
+INSERT INTO celebs(celeb_code, stage_name, stage_name_en, real_name, real_name_en, gender, birth_date, debut_year, country, country_en, state, city, height, weight, company) VALUES(
+'ACT', '이선균', '', '이선균', '', 'M', '1975-03-02', '1999', '대한민국', 'South Korea', '서울', '송파구', 180, NULL, '호두앤유 엔터테인먼트');
+INSERT INTO celebs(celeb_code, stage_name, stage_name_en, real_name, real_name_en, gender, birth_date, debut_year, country, country_en, state, city, height, weight, company) VALUES(
+'ACT', '이지은', '', '이지은', '', 'F', '1993-05-16', '2008', '대한민국', 'South Korea', '경기도', '광주시', 161.8, NULL, '이담 엔터테인먼트');
+INSERT INTO celebs(celeb_code, stage_name, stage_name_en, real_name, real_name_en, gender, birth_date, debut_year, country, country_en, state, city, height, weight, company) VALUES(
+'ACT', '고두심', '', '고두심', '', 'F', '1951-05-22', '1972', '대한민국', 'South Korea', '제주도', '제주시', 160, NULL, '맥스파워 엔터테인먼트');
+INSERT INTO celebs(celeb_code, stage_name, stage_name_en, real_name, real_name_en, gender, birth_date, debut_year, country, country_en, state, city, height, weight, company) VALUES(
+'ACT', '박호산', '', '박정환', '', 'M', '1972-10-18', '1997', '대한민국', 'South Korea', '', '', 178, 70, '라이트하우스');
+INSERT INTO celebs(celeb_code, stage_name, stage_name_en, real_name, real_name_en, gender, birth_date, debut_year, country, country_en, state, city, height, weight, company) VALUES(
+'ACT', '송새벽', '', '송새벽', '', 'M', '1979-12-26', '1998', '대한민국', 'South Korea', '전라북도', '군산시', 179, NULL, '스튜디오 산타클로스 엔터테인먼트');
+INSERT INTO celebs(celeb_code, stage_name, stage_name_en, real_name, real_name_en, gender, birth_date, debut_year, country, country_en, state, city, height, weight, company) VALUES(
+'ACT', '이지아', '', '김지아', '', 'F', '1978-08-06', '2007', '대한민국', 'South Korea', '서울', '송파구', 168, NULL, 'BH 엔터테인먼트');
+INSERT INTO celebs(celeb_code, stage_name, stage_name_en, real_name, real_name_en, gender, birth_date, debut_year, country, country_en, state, city, height, weight, company) VALUES(
+'ACT', '정영주', '', '정영주', '', 'F', '1971-05-23', '', '대한민국', 'South Korea', '', '', 169, NULL, '브이컴퍼니');
+INSERT INTO celebs(celeb_code, stage_name, stage_name_en, real_name, real_name_en, gender, birth_date, debut_year, country, country_en, state, city, height, weight, company) VALUES(
+'ACT', '손숙', '', '손숙', '', 'F', '1944-05-13', '', '대한민국', 'South Korea', '경상남도', '밀양시', NULL, NULL, '');
+INSERT INTO celebs(celeb_code, stage_name, stage_name_en, real_name, real_name_en, gender, birth_date, debut_year, country, country_en, state, city, height, weight, company) VALUES(
+'ACT', '권나라', '', '권아윤', '', 'F', '1991-03-13', '2012', '대한민국', 'South Korea', '경기도', '성남시', 172, NULL, 'A-MAN 프로젝트');
+INSERT INTO celebs(celeb_code, stage_name, stage_name_en, real_name, real_name_en, gender, birth_date, debut_year, country, country_en, state, city, height, weight, company) VALUES(
+'ACT', '오나라', '', '오나라', '', 'F', '1974-10-26', '1997', '대한민국', 'South Korea', '경기도', '하남시', 164, NULL, '매니지먼트 에이엠나인');
--- db/ddl/celebs.sql
+++ db/ddl/celebs.sql
@@ -1,19 +1,20 @@
 CREATE TABLE IF NOT EXISTS celebs(
     celeb_id      INT           NOT NULL AUTO_INCREMENT
-  , celeb_code    CHAR(3)       NULL
+  , celeb_code    CHAR(3)       NOT NULL
   , stage_name    VARCHAR(100)  NOT NULL
   , stage_name_en VARCHAR(100)  NULL
   , real_name     VARCHAR(100)  NOT NULL
   , real_name_en  VARCHAR(100)  NULL
   , gender        CHAR(1)       NULL
   , birth_date    DATE          NULL
-  , debut_date    DATE          NULL
+  , debut_year    VARCHAR(4)    NULL
   , country       VARCHAR(100)  NULL
-  , region        VARCHAR(100)  NULL
+  , country_en    VARCHAR(100)  NULL
+  , state         VARCHAR(100)  NULL
+  , city          VARCHAR(100)  NULL
   , height        DECIMAL(5,2)  NULL
   , weight        DECIMAL(5,2)  NULL
-  , company       VARCHAR(200)  NULL
-  , groups        VARCHAR(500)  NULL
+  , company       VARCHAR(500)  NULL
   , updated       DATETIME      NOT NULL  DEFAULT NOW()
   , PRIMARY KEY(celeb_id)
   , UNIQUE INDEX uindex_celeb_uid (celeb_code, celeb_id)
