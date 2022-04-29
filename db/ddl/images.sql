db21caabfaea9ffe5f39fc24b9509e44a130a91f
younghun.yun <younghun29.yun@gmail.com>
2022-04-29 17:44:42 +0900

    commit

--- db/ddl/images.sql
+++ db/ddl/images.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS images (
-    file_seq               int           NOT NULL AUTO_INCREMENT
-  , ref_id                 VARCHAR(10)   NULL
-  , original_filename      VARCHAR(1000) NOT NULL
-  , upload_filename        VARCHAR(1000) NOT NULL
+    file_seq                int           NOT NULL AUTO_INCREMENT
+  , ref_id                  VARCHAR(10)   NULL
+  , original_filename       VARCHAR(1000) NOT NULL
+  , upload_filename         VARCHAR(1000) NOT NULL
   , upload_path_filename    VARCHAR(2000) NOT NULL
   , resource_path_filename  VARCHAR(2000) NOT NULL
   , size_byte               INT           NULL
